270224727c7e877039769878f854248e27fe39d3
Oleksii Beizerov <oleksii.beizerov@gmail.com>
2024-02-19 01:41:29 +0200

    test:
    Cover by unit tests PUT endpoint cases;

--- src/test/java/com/evri/interview/controller/CourierControllerTest.java
+++ src/test/java/com/evri/interview/controller/CourierControllerTest.java
@@ -1,6 +1,7 @@
 package com.evri.interview.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -16,7 +17,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.evri.interview.exception.ResourceNotFoundException;
 import com.evri.interview.model.Courier;
+import com.evri.interview.request.CourierRequestBody;
 import com.evri.interview.service.CourierService;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,5 +46,50 @@ class CourierControllerTest {
         assertEquals(mockCouriersList, responseEntity.getBody());
 
         verify(courierService, times(1)).getAllCouriers();
+    }
+
+    @Test
+    void testUpdateCourierById_Success() {
+        long courierIdForUpdate = 1L;
+        
+        CourierRequestBody mockCourierRequestBody = CourierRequestBody.builder().build();
+        Courier mockCourier = Courier.builder().build();
+
+        when(courierService.updateCourierById(courierIdForUpdate, mockCourierRequestBody)).thenReturn(mockCourier);
+
+        ResponseEntity<Courier> responseEntity = courierController.updateCourierById(
+            courierIdForUpdate, mockCourierRequestBody
+        );
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(mockCourier, responseEntity.getBody());
+
+        verify(courierService, times(1))
+            .updateCourierById(courierIdForUpdate, mockCourierRequestBody);
+    }
+
+    @Test
+    void testUpdateCourierById_CourierNotFound() { 
+        long notExistCourierId = 1000L;
+        
+        CourierRequestBody mockCourierRequestBody = CourierRequestBody.builder().build();
+
+        when(courierService.updateCourierById(notExistCourierId, mockCourierRequestBody))
+            .thenThrow(
+                new ResourceNotFoundException(
+                    String.format("Not found courier with courierId: %s", notExistCourierId)
+                )
+            )
+        ;
+        
+        ResourceNotFoundException resourceNotFoundException = assertThrows(
+            ResourceNotFoundException.class,
+            () -> courierController.updateCourierById(notExistCourierId, mockCourierRequestBody)
+        );
+            
+        assertEquals(
+            String.format("Not found courier with courierId: %s", notExistCourierId),
+            resourceNotFoundException.getMessage()
+        );
     }
 }
