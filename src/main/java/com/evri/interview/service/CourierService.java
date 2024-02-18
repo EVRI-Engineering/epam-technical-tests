82225201a8dac409e181f059ef44a839c2a70419
Oleksii Beizerov <oleksii.beizerov@gmail.com>
2024-02-19 01:06:24 +0200

    feat: add updateCourierById method;

--- src/main/java/com/evri/interview/service/CourierService.java
+++ src/main/java/com/evri/interview/service/CourierService.java
@@ -1,9 +1,13 @@
 package com.evri.interview.service;
 
+import com.evri.interview.exception.ResourceNotFoundException;
 import com.evri.interview.model.Courier;
 import com.evri.interview.repository.CourierRepository;
+import com.evri.interview.request.CourierRequestBody;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,5 +24,20 @@ public class CourierService {
                 .stream()
                 .map(courierTransformer::toCourier)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Courier updateCourierById(long courierId, CourierRequestBody courierRequestBody) {
+        return courierTransformer.toCourier(
+            repository.findById(courierId).map(courierEntit -> {
+                courierEntit.setFirstName(courierRequestBody.getFirstName());
+                courierEntit.setLastName(courierRequestBody.getLastName());
+                courierEntit.setActive(courierRequestBody.isActive());
+
+                return repository.save(courierEntit);
+            }).orElseThrow(
+                () -> new ResourceNotFoundException(String.format("Not found courier with courierId: %s", courierId))
+            )
+        );
     }
 }
