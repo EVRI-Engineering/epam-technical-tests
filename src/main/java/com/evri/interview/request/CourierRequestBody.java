72925cdf2d9fda9f6b35f4cca2eabb884bf8357f
Oleksii Beizerov <oleksii.beizerov@gmail.com>
2024-02-19 23:40:34 +0200

    feat:
      Add validation pattern for first and last name in CourierRequestBody;

--- src/main/java/com/evri/interview/request/CourierRequestBody.java
+++ src/main/java/com/evri/interview/request/CourierRequestBody.java
@@ -1,6 +1,7 @@
 package com.evri.interview.request;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +11,11 @@ import lombok.Data;
 public class CourierRequestBody {
 
     @NotBlank(message = "firstName is required")
+    @Pattern(regexp = "^[A-Z][a-z]*$", message = "Invalid firstName format")
     String firstName;
 
     @NotBlank(message = "lastName is required")
+    @Pattern(regexp = "^[A-Z][a-z]*$", message = "Invalid lastName format")
     String lastName;
 
     boolean active;
