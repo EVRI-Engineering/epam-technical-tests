ca808c9e5a0d2f2d8699b3f089a80a8154f94532
Oleksii Beizerov <oleksii.beizerov@gmail.com>
2024-02-19 22:53:04 +0200

    feat:
      Add ValidationHandler;

---
+++ src/main/java/com/evri/interview/exception/handler/ValidationHandler.java
@@ -0,0 +1,33 @@
+package com.evri.interview.exception.handler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ValidationHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException methodArgumentNotValidException,
+            HttpHeaders headers, HttpStatus status, WebRequest request
+    ) {
+        Map<String, String> errors = new HashMap<>();
+
+        methodArgumentNotValidException.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
