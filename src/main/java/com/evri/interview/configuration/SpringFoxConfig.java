574ab151e769c6a4b477289416fd95fd4d2683e1
Oleksii Beizerov <oleksii.beizerov@gmail.com>
2024-02-19 23:41:55 +0200

    Refactoring

--- src/main/java/com/evri/interview/configuration/SpringFoxConfig.java
+++ src/main/java/com/evri/interview/configuration/SpringFoxConfig.java
@@ -10,7 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SpringFoxConfig {
     @Bean
-    public Docket api() {
+    Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.evri.interview.controller"))
