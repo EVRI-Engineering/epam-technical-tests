4110abbb6051e1161903e6f11aeb92c69c8d45a1
Dmytro Zinchuk1 <Dmytro.Zinchuk@ge.com>
2024-02-23 14:03:08 +0200

    #8 changed line separator

--- src/main/java/com/evri/interview/model/Courier.java
+++ src/main/java/com/evri/interview/model/Courier.java
@@ -1,1 +1,12 @@
-package com.evri.interview.model;import lombok.Builder;import lombok.Data;@Data@Builderpublic class Courier {    long id;    String name;    boolean active;}
+package com.evri.interview.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Courier {
+    long id;
+    String name;
+    boolean active;
+}
